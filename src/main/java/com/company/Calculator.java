f0915e5d0f5bd22cfb2aadda32a6206bd2c4f0dc
ShinkevichAnton <shinkevichanton94@gmail.com>
2020-09-03 23:32:56 +0300

    2

--- src/main/java/com/company/Calculator.java
+++ src/main/java/com/company/Calculator.java
@@ -10,13 +10,11 @@ public class Calculator {
     }
 
     public long multiply(int a, int b) {
-        return a*b;
+        return a * b;
     }
 
     public double division(int a, int b) {
-        if (b!=0){
-            return a/b;
-        }
-        return 0;
+
+        return a / b;
     }
 }
